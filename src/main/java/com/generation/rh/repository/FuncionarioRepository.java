18a3e4e00d06738ecab5e7ebeb10b6c221e4e2df
Jessica Gomes <J.gomesss@outlook.com>
2025-01-15 12:12:19 -0300

    primeiro commit

---
+++ src/main/java/com/generation/rh/repository/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package com.generation.rh.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.rh.model.Funcionario;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
+
+}
